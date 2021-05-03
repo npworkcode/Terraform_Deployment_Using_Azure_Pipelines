c10ac014adf38cc8f83ca8d999a74f8bf390e598
NP <np@workmail.com>
2021-05-03 19:55:06 -0400

    Updated provider variables

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
   features {}
 }
 
