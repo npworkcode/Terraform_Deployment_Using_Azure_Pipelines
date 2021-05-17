a37629bb32240bd7a72d656e0a94f444b9ec15b0
NP <np@workmail.com>
2021-05-17 08:09:51 -0400

    Changed NSG to allow HTTP and HTTPS

--- modules/networksecuritygroup/nsg.tf
+++ modules/networksecuritygroup/nsg.tf
@@ -25,6 +25,30 @@ resource "azurerm_network_security_group" "nsg" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
+
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1011
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+        security_rule {
+        name                       = "HTTPS"
+        priority                   = 1021
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
   tags = var.tags
 }
 resource "azurerm_subnet_network_security_group_association" "test" {
